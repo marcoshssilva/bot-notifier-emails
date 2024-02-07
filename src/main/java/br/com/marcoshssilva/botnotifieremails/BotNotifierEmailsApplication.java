baadacb74e6a9cb96ae35167bc19e2f9533b28d2
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-06 22:26:53 -0300

    initial project

---
+++ src/main/java/br/com/marcoshssilva/botnotifieremails/BotNotifierEmailsApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.botnotifieremails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotNotifierEmailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotNotifierEmailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcoshssilva/botnotifieremails/BotNotifierEmailsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.botnotifieremails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotNotifierEmailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
